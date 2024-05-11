a87c0e260013a45f7a4328c1c48155ab20a5ef15
AndriiPolishchuk28 <andrey.work28@gmail.com>
2024-05-11 10:04:21 +0200

    dynamic routing

--- src/app/components/status-label.tsx
+++ src/app/components/status-label.tsx
@@ -14,7 +14,11 @@ export enum Status {
   Suspended = 'suspended',
 }
 
-export function StatusLabel({ children, status, disabled }: StatusLabelProps) {
+export default function StatusLabel({
+  children,
+  status,
+  disabled,
+}: StatusLabelProps) {
   return (
     <div
       className={clsx(
