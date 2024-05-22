c36ee758c0803e9e723777f5af3a2411ceea04ab
AndriiPolishchuk28 <andrey.work28@gmail.com>
2024-05-22 15:37:53 +0200

    data fetching rtk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,11 @@
-import AddCompanyButton from './components/add-company-button';
-import { ClientComponents } from './components/client-components';
 import MagicButton from './components/magic-button';
 import { ServerComponents } from './components/server-components';
-import { ServerComponentsCopy } from './components/server-components copy';
 
 export default function Home() {
   return (
     <main>
       <h1 className="text-xl">Home page</h1>
-      <AddCompanyButton />
       <ServerComponents />
-      {/* <ClientComponents>
-        <ServerComponentsCopy />
-      </ClientComponents> */}
       <MagicButton />
     </main>
   );
