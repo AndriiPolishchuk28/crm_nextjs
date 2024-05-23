f94483a27532f5606e423a79bd7dbf7ef37fea94
AndriiPolishchuk28 <andrey.work28@gmail.com>
2024-05-23 11:00:22 +0200

    data fetching done

--- src/app/components/status-label.tsx
+++ src/app/components/status-label.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { clsx } from 'clsx';
+import clsx from 'clsx';
+import { CompanyStatus } from '@/lib/api';
 
 export interface StatusLabelProps {
   status: CompanyStatus;
   disabled?: boolean;
+  styled?: boolean;
 }
-
-import { CompanyStatus } from '@/lib/api';
 
 const labelByStatus = {
   [CompanyStatus.Active]: 'Active',
@@ -15,22 +15,29 @@ const labelByStatus = {
   [CompanyStatus.Suspended]: 'Suspended',
 };
 
-export default function StatusLabel({ status, disabled }: StatusLabelProps) {
+export default function StatusLabel({
+  status,
+  disabled,
+  styled = true,
+}: StatusLabelProps) {
+  const label = labelByStatus[status];
+  if (!styled) return <>{label}</>;
+
   return (
     <div
       className={clsx(
+        'inline-flex items-center py-1 px-3.5 rounded-3xl text-sm font-medium',
         status === CompanyStatus.Active && 'text-green-700 bg-green-100',
         status === CompanyStatus.NotActive && 'text-red-700 bg-red-100',
         status === CompanyStatus.Pending && 'text-orange-700 bg-orange-100',
         status === CompanyStatus.Suspended && 'text-blue-700 bg-blue-100',
-        'inline-flex items-center py-1 px-3.5 rounded-3xl text-sm font-medium',
         {
           ['opacity-75 cursor-not-allowed']: disabled,
         },
       )}
     >
       <div className="w-1 h-1 mr-2 rounded-full bg-current" />
-      {labelByStatus[status]}
+      {label}
     </div>
   );
 }
